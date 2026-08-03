74030eb87600e6cbca5276ece8bf1fb73e71d6a3
rajputkamlesh4747-lgtm <rajputkamlesh4747@gmail.com>
2026-08-03 20:46:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Global Services Agency Loaded");
 
-    const button = document.querySelector(".hero a");
-
-    button.addEventListener("click", (e) => {
-        e.preventDefault();
-        alert("Welcome to Global Services Agency! More features are coming soon.");
-    });
-});
