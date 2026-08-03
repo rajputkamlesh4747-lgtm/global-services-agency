1323ff210034924c71eccb8304ac63263ba252ec
rajputkamlesh4747-lgtm <rajputkamlesh4747@gmail.com>
2026-08-03 20:49:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+// Global Services Agency
 
+document.addEventListener("DOMContentLoaded", function () {
+
+console.log("Website Loaded Successfully");
+
+const buttons = document.querySelectorAll("a");
+
+buttons.forEach(button => {
+
+button.addEventListener("click", function(e){
+
+if(this.getAttribute("href")==="#"){
+
+e.preventDefault();
+
+alert("This page will be available soon!");
+
+}
+
+});
+
+});
+
+window.addEventListener("scroll",function(){
+
+const header=document.querySelector(".header");
+
+if(window.scrollY>50){
+
+header.style.background="#000";
+
+}else{
+
+header.style.background="rgba(0,0,0,.65)";
+
+}
+
+});
+
+});
